88939e0b7ec38b3c27ad12eff9803621287ba54d
18222478537@163.com <18222478537@163.com>
2019-05-17 16:56:45 +0800

    header中间部分

---
+++ src/common/header/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {HeaderWrapper,Logo,Nav, NavItem} from './style'
+class Header extends React.Component{
+    render() {
+        return (
+            <HeaderWrapper>
+                <Logo href="/"/>
+                <Nav>
+                   <NavItem className='left active'>首页</NavItem>
+                   <NavItem className='left'>下载App</NavItem>
+                   <NavItem className='right'>Aa</NavItem>
+                   <NavItem className='right'>登陆</NavItem>
+                </Nav>
+            </HeaderWrapper>
+        )
+    } 
+}
+
+export default Header
