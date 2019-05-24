8668dd2e6be90881285f3394f2a6ff875c2e9d76
18222478537@163.com <18222478537@163.com>
2019-05-24 19:04:32 +0800

    首页文章列表制作

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,10 +1,32 @@
 import React from 'react'
-
+import {connect} from 'react-redux'
+import {ListItem, ListInfo} from '../style'
 class List extends React.Component{
     render(){
         return (
-            <div>List</div>
+            <div>
+                {
+                    this.props.list.map((item) => {
+                        return (
+                            <ListItem key={item.get('id')}>
+                                <img className='pic' alt="wuyiyi" src={item.get('imgUrl')}/>
+                                <ListInfo>
+                                    <h3 className='title'>{item.get('title')}</h3>
+                                    <p className='desc'>{item.get('desc')}</p>
+                                </ListInfo>
+                            </ListItem>
+                        )
+                        
+                    })
+                }
+           
+           </div>
         )
     }
 }
-export default List
+const mapState = (state) => {
+    return {
+        list: state.get('home').get('articleList')
+    }
+}
+export default connect(mapState, null)(List);
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -24,9 +24,10 @@ export const TopicWrapper= styled.div`
     overflow: hidden;
     padding: 20px 0 10px 0;
     margin-left: -18px;
+    border-bottom: 1px solid #dcdcdc;
 `
 export const TopicItem= styled.div`
-    float: left:
+    float: left;
     height: 32px;
     line-height: 32px;
     margin-left: 18px;
@@ -38,11 +39,40 @@ export const TopicItem= styled.div`
     border: 1px solid #dcdcdc;
     border-radius: 4px;
     .topic-pic{
-        margin-right: 10px;
+        
         display:block;
         float: left;
         width: 32px;
         height: 32px;
+        margin-right: 10px;
     }
 
+`
+export const ListItem = styled.div`
+    overflow: hidden;
+    padding: 20px 0;
+    border-bottom: 1px solid #dcdcdc;
+    img{
+        border-radius: 10px;
+        padding: 20px 0;
+        display: block;
+        width: 125px;
+        height: 100px;
+        float: right;
+    }
+`
+export const ListInfo = styled.div`
+    width: 500px;
+    float: left;
+    .title{
+        line-height:27px;
+        font-size:18px;
+        font-weight: bold;
+        color: #333;
+    }
+    .desc{
+        line-height: 24px;
+        font-size: 13px;
+        color: #999;
+    }
 `
